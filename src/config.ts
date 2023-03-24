385c60b557f6ccd1c2dae619907dc868e7f4c8b4
Kyle Gordy <kyletgordy@gmail.com>
2023-03-24 11:50:59 -0400

    Add Astro SEO, add basic open graph image

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@
 
 export const SITE_TITLE = "Kyle Gordy";
 export const SITE_DESCRIPTION = "I'm a multifaceted designer/developer with 10+ years of experience.";
+export const SITE_OG_IMG = "https://kgordy.com/assets/og-image.png";
 export const CHAT_BOOKING_URL = "https://calendly.com/kylegordy/30min";
