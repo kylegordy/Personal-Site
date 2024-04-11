6da98219b85feb9fc91d1d9fb82d63e28130cd12
Kyle Gordy <kyletgordy@gmail.com>
2024-04-11 16:28:42 -0400

    Tweak site descriptions and improve non-svg favicon support

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Kyle Gordy";
-export const SITE_DESCRIPTION = "I'm a multifaceted UI Designer/Frontend Developer with 10+ years of experience.";
+export const SITE_DESCRIPTION =
+  "I'm a multifaceted UI/UX Designer with 10+ years of experience.";
 export const SITE_OG_IMG = "https://kgordy.com/assets/og-image.png";
 export const CHAT_BOOKING_URL = "https://calendly.com/kylegordy/30min";
