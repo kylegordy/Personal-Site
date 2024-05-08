0d0855428e7e6c5fc9442c3e466b589f6a991e5e
Kyle Gordy <kyletgordy@gmail.com>
2024-05-08 11:03:11 -0400

    Tweak site description, fix Astro SEO template

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Kyle Gordy";
-export const SITE_DESCRIPTION =
-  "I'm a multifaceted UI/UX Designer with 10+ years of experience.";
+export const SITE_DESCRIPTION = "UI/UX Designer Who Codes";
 export const SITE_OG_IMG = "https://kgordy.com/assets/og-image.png";
 export const CHAT_BOOKING_URL = "https://calendly.com/kylegordy/30min";
